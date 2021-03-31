0d6f9bdb45c7bc6cd1fba9206c98dd1d7d166bdf
Eluir Monteiro <eluirjunior@gmail.com>
2021-03-31 20:57:28 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,8 @@
+nome = 'Eluir'
+sobrenome = 'De Almeida Monteiro Junior'
+idade = 23
+peso = 74.4
+
+print('Nome:' + nome + ' ' + sobrenome + '\nIdade:' + str(idade) + '\nPeso:' + str(peso))
+print('----------------------------------------------')
+print(f'Nome: {nome} {sobrenome}\nIdade: {idade}\nPeso: {peso}')
