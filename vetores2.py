b8dbf6b222ab114318ffcda66679e3144a3ea3c8
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-01 20:35:58 -0300

    vetores2

---
+++ vetores2.py
@@ -0,0 +1,6 @@
+lista_par = [0, 2, 4, 6]
+lista_impar = [1, 3, 5, 7, 9]
+lista_multiplos_5 = [5, 10, 15, 20]
+
+lista_numeros = [lista_par, lista_impar, lista_multiplos_5]
+print(lista_numeros[1][3])
