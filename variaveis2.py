3ad4b6b9f409e02faa7a519248619022eaf63934
Eluir Monteiro <eluirjunior@gmail.com>
2021-03-31 21:05:34 -0300

    variaveis com input

---
+++ variaveis2.py
@@ -0,0 +1,8 @@
+nome = input('Qual é seu nome, padwan? ')
+sobrenome = input('Qual é seu sobrenome? ')
+idade = input('Qual sua idade? ')
+peso = input('Qual seu peso? ')
+
+
+print('----------------------------------------------')
+print(f'Nome: {nome} {sobrenome}\nIdade: {idade}\nPeso: {peso}')
