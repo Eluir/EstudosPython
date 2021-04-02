ec8811f2b0ffd5814bc2e4a51b97866712c3154a
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-02 19:08:34 -0300

    resolucao exec 1

---
+++ exercicios.py
@@ -0,0 +1,7 @@
+nome = input('Qual seu nome?\n')
+idade = int(input('Qual sua idade?\n'))
+
+if idade >= 18:
+    print('Você já pode tirar a CNH!')
+else:
+    print('Eita! Infelizamente ainda você não pode tirar a CNH!')
