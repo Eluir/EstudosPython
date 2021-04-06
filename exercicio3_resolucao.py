5c24ec38306aef523c8bc782d08ac5abea1bc040
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-05 23:42:50 -0300

    resolucao exercício 3

---
+++ exercicio3_resolucao.py
@@ -0,0 +1,16 @@
+nome = str(input('Qual seu nome?\n'))
+nota1 = float(input('Qual sua nota do 1º bimestre?\n'))
+nota2 = float(input('Qual sua nota do 2º bimestre?\n'))
+nota3 = float(input('Qual sua nota do 3º bimestre?\n'))
+nota4 = float(input('Qual sua nota do 4º bimestre?\n'))
+soma_notas = ((nota1 + nota2 + nota3 + nota4) / 4)
+
+if soma_notas <= 40:
+    print('Reprovado')
+elif soma_notas > 40 and soma_notas <= 60:
+    print('Recuperação')
+elif soma_notas > 60 and soma_notas <= 100:
+    print('Aprovado')
+
+
+#Perguntar como faz pra usar o resultado do if em um print adicionando um texto
