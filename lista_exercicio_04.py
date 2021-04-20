3aaa78bbce4156499fbf38074e6f3b58b5ed8f74
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-20 00:09:01 -0300

    exercicio 4 e5

---
+++ lista_exercicio_04.py
@@ -0,0 +1,15 @@
+h = 3
+b = 3
+j = 8
+l = 7
+
+if h < 6:
+    if b < 5:
+        if j <= 5:
+            k = 8
+        else:
+            k = 1
+        l = k + 8
+        l = l + 1
+
+print(j + l - h)
---
+++ lista_exercicio_05.py
@@ -0,0 +1,14 @@
+m = 3
+n = 3
+p = 5
+
+if m < 8:
+    if n >= 3:
+        if p <= 4:
+            m = m + 3
+        else:
+            n = n + 5
+            p = p + 5
+        m = n + p
+
+print(p + m + n)
