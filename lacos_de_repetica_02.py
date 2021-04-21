4e4425335be4ecf6c66aefb99ad8592bfe574f72
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-20 21:13:31 -0300

    lacos de repeticao com while

---
+++ lacos_de_repetica_02.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um numero\n'))
+contador = 0
+
+while num != contador:
+    if contador % 2 == 0:
+        print(f'{contador} é par')
+    else:
+        print(f'{contador} não é par')
+    contador += 1
