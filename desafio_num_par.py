e920a803a7211cb5e9a3ae76455920a5400abfdf
Eluir Monteiro <eluirjunior@gmail.com>
2021-04-26 22:59:24 -0300

    resolucao desafio numeros pares

---
+++ desafio_num_par.py
@@ -0,0 +1,9 @@
+num = int(input('Informe um número aí meu chapa!\n'))
+count = 0
+sum = 0
+
+while num != count:
+    if count != 0 and count % 2 == 0:
+        sum += count
+    count += 1
+print(f'Valor acumulado dos numeros pares é: {sum}')
